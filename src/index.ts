d8e98fb58d383ecb6a8613b7cee225aa23389271
ciriousjoker <ciriousjoker@gmail.com>
2022-03-17 03:17:26 +0100

    Feature: await completer;

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-export class Completer<T = void> {
+export class Completer<T = void> implements Promise<T> {
+  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null): Promise<TResult1 | TResult2> {
+    return this.promise.then(onfulfilled, onrejected);
+    // return this as unknown as Promise<TResult1 | TResult2>;
+  }
+
+  catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | null): Promise<T | TResult> {
+    return this.promise.catch(onrejected);
+    // return this;
+  }
+
+  finally(onfinally?: (() => void) | null): Promise<T> {
+    this.promise.finally(onfinally);
+    return this;
+  }
+
+  get [Symbol.toStringTag]() {
+    return 'Completer';
+  }
+
   public readonly promise = new Promise<T>((resolve, reject) => {
-    this._complete = resolve;
-    this._completeError = reject;
+    this._resolve = resolve;
+    this._reject = reject;
   });
 
   public get isCompleted(): boolean {
@@ -9,18 +28,18 @@ export class Completer<T = void> {
   }
   private _isCompleted = false;
 
-  private _complete!: (value: PromiseLike<T> | T) => void;
-  private _completeError!: (reason?: any) => void;
+  private _resolve!: (value: PromiseLike<T> | T) => void;
+  private _reject!: (reason?: any) => void;
 
   public complete(value: T | PromiseLike<T>) {
     if (this._isCompleted) return;
-    this._complete.call(this, value);
+    this._resolve(value);
     this._isCompleted = true;
   }
 
-  public completeError(reason?: any) {
+  public async completeError(reason?: any) {
     if (this._isCompleted) return;
-    this._completeError.call(this, reason);
+    this._reject(reason);
     this._isCompleted = true;
   }
 }
